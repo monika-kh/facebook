10b28d8b7b93f02ac2c6e984eaefa9729c085f0a
monika-kh <khasdevmonika@gmail.com>
2020-01-06 17:01:58 +0530

    Email settings.

--- facebook/settings.py
+++ facebook/settings.py
@@ -130,3 +130,12 @@ REST_FRAMEWORK = {
     ],
 }
 
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Sending mail form this email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'khasdevmonika@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwerty12345moni1993'
