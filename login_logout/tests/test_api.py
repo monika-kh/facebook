04c15255897ef65221b761db28e6d3107c46c875
monika-kh <khasdevmonika@gmail.com>
2020-01-23 19:22:10 +0530

    test_api.py.

--- login_logout/tests/test_api.py
+++ login_logout/tests/test_api.py
@@ -1,16 +1,23 @@
-# from django.urls import reverse
-# from rest_framework.test import APITestCase
-# from rest_framework.authtoken.models import Token
-# from rest_framework.test import force_authenticate
-#
-# from .. models import Facebook
-# from login_logout.serializers import FacebookSerializer
-#
-#
-# class LoginTest(APITestCase):
-#     def setUp(self):
-#         self.user = Facebook.objects.create_user(
-#             username='user@foo.com', email='user@foo.com', password='top_secret')
-#         breakpoint()
-#         self.token = Token.objects.create(user=self.user)
-#         self.token.save()
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import force_authenticate
+
+from .. models import Facebook
+from login_logout.serializers import FacebookSerializer
+
+
+class LoginTest(APITestCase):
+    def setUp(self):
+        self.user = Facebook.objects.create_user(
+            username='user@foo.com', password='top_secret')
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+        breakpoint()
+
+    # def test_token_auth(self):
+    #     force_authenticate(request, token=self.token.key)
+    #     view = views.EndpointViewSet.as_view({'get': 'list'})
+    #     response = view(request)
+    #     self.assertEqual(response.status_code, 200)
+    #     json_response = json.loads(response.render().content)['results']
