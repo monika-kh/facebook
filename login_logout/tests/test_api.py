10bd85fdca7bc9235e59f23e772cd65db3cf9122
monika-kh <khasdevmonika@gmail.com>
2020-01-24 18:39:31 +0530

    test_api

--- login_logout/tests/test_api.py
+++ login_logout/tests/test_api.py
@@ -1,23 +1,28 @@
-from django.urls import reverse
-from rest_framework.test import APITestCase
+# from django.urls import reverse
+# from django.contrib.auth.models import AbstractUser
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
 from rest_framework.authtoken.models import Token
 from rest_framework.test import force_authenticate
+# from rest_framework.permissions import IsAuthenticated
 
-from .. models import Facebook
-from login_logout.serializers import FacebookSerializer
+from .. import views
+from ..models import Facebook
+# from login_logout.serializers import FacebookSerializer
+
+# from ..views import LoginView
 
 
 class LoginTest(APITestCase):
     def setUp(self):
-        self.user = Facebook.objects.create_user(
-            username='user@foo.com', password='top_secret')
+        self.factory = APIRequestFactory()
+        self.user = Facebook.objects.create_user(username='test', password='test123')
         self.token = Token.objects.create(user=self.user)
         self.token.save()
-        breakpoint()
 
-    # def test_token_auth(self):
-    #     force_authenticate(request, token=self.token.key)
-    #     view = views.EndpointViewSet.as_view({'get': 'list'})
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, 200)
-    #     json_response = json.loads(response.render().content)['results']
+    def test_token_auth(self):
+        request = self.factory.get('/fb/login/')
+        force_authenticate(request, user=self.user, token=self.user.auth_token)
+        view = views.LoginView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
---
+++ login_logout/tests/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from college_app.views import CollegeView
+from rest_framework.test import APITestCase
+
+from college_app import views
+
+
+class TestLoginUrls(SimpleTestCase):
+breakpoint()
